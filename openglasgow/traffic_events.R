4a8355d998cb6a4cdd801281d22d44030c196708
Victoria Catterson <vic@cowlet.org>
2013-10-20 22:03:41 +0100

    Add R code to parse Open Glasgow traffic data.

---
+++ openglasgow/traffic_events.R
@@ -0,0 +1,44 @@
+# Pull data from Open Glasgow's traffic events feed, and parse
+# into a CSV file of interesting traffic information.
+# http://data.glasgow.gov.uk/dataset/glasgow-road-network-traffic-events-feed
+
+library("RJSONIO")
+
+# Parse a situationRecord for its interesting details
+parse.situation <- function(sit)
+{
+    # Location descriptor position varies between a single point, and a list of points.
+    # Try to fetch the descriptor. This will work if it's a single point.
+    loc.descriptor <- sit$situationRecord$groupOfLocations$locationContainedInGroup$tpegpointLocation$point$name$descriptor
+    
+    # If it is a list of points, the descriptor will be null instead:
+    if (is.null(loc.descriptor))
+    {
+        # In this case, use the first point's descriptor instead.
+        points.list <- sit$situationRecord$groupOfLocations$locationContainedInGroup$tpegpointLocation$point$name
+        loc.descriptor <- points.list[[1]]$descriptor
+    }
+
+    return (data.frame(as.POSIXct(sit$situationRecord$situationRecordCreationTime),
+                       as.POSIXct(sit$situationRecord$validity$validityTimeSpecification[1]),
+                       as.POSIXct(sit$situationRecord$validity$validityTimeSpecification[2]),
+                       sit$situationRecord$validity$validityStatus,
+                       sit$situationRecord[[length(sit$situationRecord)]],
+                       loc.descriptor,
+                       as.numeric(sit$situationRecord$groupOfLocations$locationContainedInGroup$tpegpointLocation$point$pointCoordinates[1]),
+                       as.numeric(sit$situationRecord$groupOfLocations$locationContainedInGroup$tpegpointLocation$point$pointCoordinates[2]),
+                       sit$situationRecord$nonGeneralPublicComment$comment,
+                       row.names=NULL))
+}
+
+# The source file has a missing newline at the end, so suppress the warning
+json <- readLines(url("http://dashboard.glasgow.gov.uk/api/live/trafficEvents.php?type=json"), warn=FALSE)
+data.json <- fromJSON(json)
+
+# Apply parse.situation to all traffic event "situations". These are stored in the JSON
+# data as an array of objects at `payloadPublication.situation`.
+data <- do.call(rbind, Map(parse.situation, data.json$payloadPublication$situation))
+colnames(data) <- c("creation", "start", "end", "status", "event", "location", "latitude", "longitude", "description")
+
+# Store the parsed information
+write.table(data, file="traffic_events.csv", sep=",", row.names=FALSE)
